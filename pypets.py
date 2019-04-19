90afad99cf1a086e1ce62eea9055c199a5936fa4
drhorrible <46657792+kurczak-mielony@users.noreply.github.com>
2019-04-19 11:50:02 -0500

    Game Update 1
    Added "support" for automatically restarting the game in Windows. Also added the feature to edit usernames.

--- pypets.py
+++ pypets.py
@@ -1,42 +1,33 @@
 #PyPets
 #By Kurczak Mielony
 import random, time, sys, sqlite3, os
+
 def apsched():
     try:
         from apscheduler.schedulers.background import BackgroundScheduler
         return False
     except ModuleNotFoundError:
         print("APScheduler, a vital component to the game, is being installed. After its installation, Python will exit.\nRestarting the game will not yield this message again unless there was an error in the installation.\nThank you.\n\n")
-        os.system('pip3 install --user apscheduler')
-        if os.name != "nt":
-            print("The game will try to automatically relaunch itself. Please wait."+("\n"*3))
-            return True
-        else: exit()
+        os.system("py -3 -m pip install apscheduler > apscheduler_install.txt" if os.name == 'nt' else "pip3 install --user apscheduler > apscheduler_install.txt")
+        print("The game will try to automatically relaunch itself. Please wait.\n\n\n")
+        return True
+
 flag = apsched()
-if flag: os.system(f"sleep 2 && python {os.path.abspath(__file__)}")
+if flag: os.system(f"TIMEOUT 2 > nul && python {os.path.abspath(__file__)}" if os.name == 'nt' else f"sleep 2 && python {os.path.abspath(__file__)}")
 else: from apscheduler.schedulers.background import BackgroundScheduler #This backwards approach seems to work on installing APScheduler even on computers without it, as well as removing any name errors.
 
 hunger = random.randint(80,100)
 happiness = random.randint(80,100)
 dirtiness = random.randint(50, 100)
 sick = False; sicknessfactor = 2
+#Right now, these stats don't mean anything. But later on, when the pets portion part of the database is complete,
+#these will be saved per pet, and no longer just be randomly generated.
 
-username = ""
+
+username = "" #Done to prevent name errors later on.
 
 conn = sqlite3.connect('pypets.db')
 c = conn.cursor()
-
-def background():
-    global hunger
-    if hunger != 0:
-        value = "Your pet lost 5 hunger.\n"
-        if hunger < 60: value+="Your pet is very hungry. You should feed it!\n"
-        hunger-=5
-        print(value)
-    else: return
-
-def selection(inpt, num, letter):
-        if inpt == str(num) or inpt.upper().strip() == letter: return True
 
 def statgain(stat, statgain, adjective):
     if stat+statgain > 100:
@@ -46,6 +37,40 @@ def statgain(stat, statgain, adjective):
     if statgain == 1: statgain = f"{statgain} {adjective} point"
     else: statgain = f"{statgain} {adjective} points"
     return statgain, stat
+
+def feedpet():
+    global hunger
+    adj = "food"
+    if hunger == 100:
+        print("Your pet is already at max hunger.")
+        return
+    food = random.choice(("a nice steak", "a plate of pancakes", "a fish"))
+    hungergain = random.randint(1,10)
+    hungergain, hunger = statgain(hunger, hungergain, adj)
+    print(f"You fed your pet {food} and it gained {hungergain}! It is now at {hunger}/100 {adj} points.\n")
+
+def play():
+    global happiness
+    adj = "happiness"
+    happinessgain = random.randint(1,10)
+    game = random.choice(("played a nice game of fetch", "chased each other around", "enjoyed the sights at the park"))
+    happinessgain, happiness = statgain(happiness, happinessgain, adj)
+    print(f"You and your pet spent some time together and you {game}. Your pet gained {happinessgain}. It is now at {happiness}/100 {adj} points.\n")
+
+def display_stats():
+    global hunger, happiness, dirtiness, sick
+    stats = f"Hunger is currently {hunger}/100\nHappiness is currently {happiness}/100\nDirtiness is currently {dirtiness}/100\n"
+    if sick: stats+="Your pet is currently sick.\n"
+    print(stats)
+
+def background():
+    global hunger
+    if hunger != 0:
+        value = "Your pet lost 5 hunger.\n"
+        if hunger < 60: value+="Your pet is very hungry. You should feed it!\n"
+        hunger-=5
+        print(value)
+    else: return
 
 def dirty():
     global dirtiness, sick, sicknessfactor
@@ -68,6 +93,8 @@ def dirty():
     display = f"Yuck! Your pet {activity} and gained {dirtinessgain}! It is now at {dirtiness}/100 {adj} points.\n"
     if sick: display+="Your pet has contracted an illness. You can remedy this by applying medication to your pet.\n"
     print(display)
+def selection(inpt, num, letter):
+        if inpt == str(num) or inpt.upper().strip() == letter: return True
 
 def userselection():
     global username
@@ -105,11 +132,19 @@ def userselection():
                 print("Please either choose the number corresponding to a user or \'C\'.")
         break
 
-def display_stats():
-    global hunger, happiness, dirtiness, sick
-    stats = f"Hunger is currently {hunger}/100\nHappiness is currently {happiness}/100\nDirtiness is currently {dirtiness}/100\n"
-    if sick: stats+="Your pet is currently sick.\n"
-    print(stats)
+def username_edit():
+    global username
+    c.execute('SELECT userid FROM pypetusers WHERE name=?', username)
+    #Normally, the syntax for inserting variables into a query is to put it into a tuple, which is still being done, but implictly.
+    #If you look at game(), you'll see that when the username is printed, it's username[0], because the data is already in a tuple!
+    userid = c.fetchone()[0]
+    newusername = input("Please choose a new username to be known as. If you'd like to cancel this operation, press enter.")
+    if newusername == "": return
+    else: c.execute('UPDATE pypetusers SET name=? WHERE userid=?', (newusername, userid))
+    print("Done. Your name has been edited.")
+    c.execute('SELECT name FROM pypetusers WHERE userid=?', (userid,))
+    username = c.fetchone()
+    conn.commit()
 
 def game():
     userselection()
@@ -121,39 +156,19 @@ def game():
         scheduler.add_job(dirty, 'interval', seconds=30, jitter=5)
         scheduler.start()
         while True:
-            choicetext = "What would you like to do?\n1. F eed your pet\n2. P lay with your pet\n3. S how current stats\n4. C lear the screen\n5. M ain menu\n"
+            choicetext = "What would you like to do?\n1. F eed your pet\n2. P lay with your pet\n3. S how current stats\n4. C lear the screen\n5. E dit your name\n6. M ain menu\n"
             if sick: choicetext+="6. G ive your pet medicine"
             choice = input(choicetext)
             if selection(choice, 1, "F"): feedpet()
             elif selection(choice, 2, "P"): play()
             elif selection(choice, 3, "S"): display_stats()
             elif selection(choice, 4, "C"): os.system('cls' if os.name == 'nt' else 'clear')
-            elif selection(choice, 5, "M"):
-                scheduler.shutdown()
-                break
+            elif selection(choice, 5, "E"): username_edit()
+            elif selection(choice, 6, "M"): scheduler.shutdown(); break
             elif selection(choice, 6, "G") and sick: sick = False; print("Your pet was cured of its ailment!\n")
             else: print("Please choose the number or letter corresponding to the activity.\n")
     except (KeyboardInterrupt, SystemExit):
             scheduler.shutdown()
-
-def play():
-    global happiness
-    adj = "happiness"
-    happinessgain = random.randint(1,10)
-    game = random.choice(("played a nice game of fetch", "chased each other around", "enjoyed the sights at the park"))
-    happinessgain, happiness = statgain(happiness, happinessgain, adj)
-    print(f"You and your pet spent some time together and you {game}. Your pet gained {happinessgain}. It is now at {happiness}/100 {adj} points.\n")
-
-def feedpet():
-    global hunger
-    adj = "food"
-    if hunger == 100:
-        print("Your pet is already at max hunger.")
-        return
-    food = random.choice(("a nice steak", "a plate of pancakes", "a fish"))
-    hungergain = random.randint(1,10)
-    hungergain, hunger = statgain(hunger, hungergain, adj)
-    print(f"You fed your pet {food} and it gained {hungergain}! It is now at {hunger}/100 {adj} points.\n")
 
 def mainmenu():
     main_menu = """ _______  __   __  _______  _______  _______  _______
